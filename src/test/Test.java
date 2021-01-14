3a6f1387ddecef8d8b5a00c746ce1102399b5487
fengyujie <fengyujie@xiaomi.com>
2021-01-14 19:07:29 +0800

    初始化jdk阅读源码

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Map map = new HashMap<>();
+
+        System.out.println("hello");
+    }
+}
